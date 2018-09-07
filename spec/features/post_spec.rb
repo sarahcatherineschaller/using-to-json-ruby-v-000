feec5e78dc7063cb1924013667098d7f36eaf14a
Sarah Schaller <sarahcschaller@gmail.com>
2018-09-07 16:59:20 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -29,7 +29,7 @@ describe 'form' do
     fill_in 'post[title]', with: "My post title"
     fill_in 'post[description]', with: "My post description"
 
-    click_on "Submit"
+    click_on "Submit Post"
 
     expect(page).to have_content("My post title")
   end
